efa1fe1ba9b6005e2f6413b37afcfa7114590f25
John <jjnrmason@me.com>
2021-03-10 22:13:27 +0000

    Enter send event

--- vscr/src/components/input-wrapper/InputWrapper.component.jsx
+++ vscr/src/components/input-wrapper/InputWrapper.component.jsx
@@ -3,10 +3,16 @@ import SendButton from '../send-button/SendButton.component'
 import "./input-wrapper.css";
 
 class InputWrapper extends Component {
+    sendMessage = (e) => {
+        if (e.key === "Enter") {
+            this.props.sendEvent();
+        }
+    };
+
     render() {
         return (
             <div className="input-wrapper__component">
-                <textarea className="input-wrapper__text-area" type="text" onChange={(event) => { this.props.onChange(event.target.value) }}></textarea>
+                <textarea onKeyUp={this.sendMessage} className="input-wrapper__text-area" type="text" onChange={(event) => { this.props.onChange(event.target.value) }}></textarea>
                 <SendButton sendEvent={this.props.sendEvent}/>
             </div>
         )
